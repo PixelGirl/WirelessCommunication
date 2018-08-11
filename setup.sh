6c7d75e689c57720d7a855924a53579476a31f67
PixelGirl <me.pixelgirl@gmail.com>
2018-08-11 17:37:06 +0200

    Modified setup.sh

--- setup.sh
+++ setup.sh
@@ -9,10 +9,10 @@ mkdir -p libs/
 
 cd libs/
 
-echo downloading CoFH Core
+echo Downloading CoFH Core...
 wget --content-disposition "https://minecraft.curseforge.com/projects/cofhcore/files/2388751/download"
 
-echo downloading Thermal Foundation
+echo Downloading Thermal Foundation...
 wget --content-disposition https://minecraft.curseforge.com/projects/thermal-foundation/files/2388753/download
 
 ${DIR}/gradlew -p "$DIR" :setupDecomWorkspace
